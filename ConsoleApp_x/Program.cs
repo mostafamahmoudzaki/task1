a523bb3241c803a4ae9611bcc36b64c9dd03b8ea
mostafamahmoudzaki <121122897+mostafamahmoudzaki@users.noreply.github.com>
2022-12-20 21:18:33 +0200

    Add files via upload

---
+++ ConsoleApp_x/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_x
+{
+    internal class Program
+    {
+        static bool isPerfect(int number)
+        {
+            int sum = 0;
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0)
+                    sum += i;
+            }
+            if (sum==number)
+                return true;
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            int x, y;
+            Console.Write("enter the first number : ");
+            x= int.Parse(Console.ReadLine());
+            Console.Write("enter the second number : ");
+            y = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n");
+
+            for (int i = x; i <=y; i++)
+            {
+                if (isPerfect(i))
+                    Console.WriteLine("{0}", i);
+            }
+        }
+    }
+}
